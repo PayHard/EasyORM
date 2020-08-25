d2785f3b46f6edf44210abb0d9f96cca043127c4
LHP <1339003591@qq.com>
2020-08-25 21:29:43 +0800

    添加单元测试

---
+++ Test.Sql/Test_SqlCommandGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Sql
+{
+    [TestClass]
+    public class Test_SqlCommandGenerator
+    {
+        public Test_SqlCommandGenerator()
+        {
+
+        }
+
+        [TestMethod]
+        public void Add()
+        {
+        }
+
+    }
+}
